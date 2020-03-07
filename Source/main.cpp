a43662264818f5dab09c8361b126ad2adc47d6ae
Paul <paulfroelich97@gmail.com>
2020-03-07 19:50:06 +0100

    modified the setup again to just work for now.

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,8 +3,7 @@
 #include <windows.h>
 #include FT_FREETYPE_H
 
-// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
 {
 	FT_Library library;
 	FT_Face face;
@@ -28,23 +27,26 @@ int main()
 		std::cout << "this font face's file couldn't be read.\n";
 	}
 
-	if((face->face_flags & FT_FACE_FLAG_SCALABLE) == FT_FACE_FLAG_SCALABLE)
-		std::cout << "This font contains outline glyphs. Note that a face can contain bitmap strikes also.\n";
+	if ((face->face_flags & FT_FACE_FLAG_SCALABLE) == FT_FACE_FLAG_SCALABLE)
+		OutputDebugString("This font contains outline glyphs. Note that a face can contain bitmap strikes also.\n");
 	if((face->face_flags & FT_FACE_FLAG_FIXED_SIZES) == FT_FACE_FLAG_FIXED_SIZES)
-		std::cout << "This font contains bitmap strikes.\n";
+		OutputDebugString("This font contains bitmap strikes.\n");
 	if ((face->face_flags & FT_FACE_FLAG_FIXED_WIDTH) == FT_FACE_FLAG_FIXED_WIDTH)
-		std::cout << "This font contains fixed width characters.\n";
+		OutputDebugString("This font contains fixed width characters.\n");
 	if((face->face_flags & FT_FACE_FLAG_SFNT) == FT_FACE_FLAG_SFNT)
-		std::cout << "This font uses the SFNT storage scheme.\n";
+		OutputDebugString("This font uses the SFNT storage scheme.\n");
 	if((face->face_flags & FT_FACE_FLAG_HORIZONTAL) == FT_FACE_FLAG_HORIZONTAL)
-		std::cout << "This font contains horizontal glyph metrics.\n";
+		OutputDebugString("This font contains horizontal glyph metrics.\n");
 	if ((face->face_flags & FT_FACE_FLAG_VERTICAL) == FT_FACE_FLAG_VERTICAL)
-		std::cout << "This font contains vertical glyph metrics.\n";
+		OutputDebugString("This font contains vertical glyph metrics.\n");
 	if ((face->face_flags & FT_FACE_FLAG_KERNING) == FT_FACE_FLAG_KERNING)
-		std::cout << "This font contains kerning information. The kerning distance can be retrieved using the function Get_FT_Kerning()\n";
+		OutputDebugString("This font contains kerning information. The kerning distance can be retrieved using the function Get_FT_Kerning()\n");
 
-	std::cout << '\n' << "The face contains " << face->num_glyphs << " glyphs.\n";
-	std::cout << '\n' << "The face uses " << face->units_per_EM << " font units per EM.\n";
+	char output_buffer[256];
+	sprintf_s(output_buffer, "\nThe face contains %d  glyphs.\n", face->num_glyphs);
+	OutputDebugString(output_buffer);
+	sprintf_s(output_buffer, "\nThe face uses %d font units per EM.\n", face->units_per_EM);
+	OutputDebugString(output_buffer);
 
-	std::cin.get();
+	return 0;
 }
